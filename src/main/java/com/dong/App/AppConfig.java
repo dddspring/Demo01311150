e4f66e25f9272b7ca40111e177e46fde4a80e467
dddspring <1457930071@qq.com>
2020-04-22 23:28:31 +0800

    实现map解析

--- src/main/java/com/dong/App/AppConfig.java
+++ src/main/java/com/dong/App/AppConfig.java
@@ -1,15 +1,25 @@
 package com.dong.App;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.dong.Aop.DongBeanFactoryProcessor;
 import com.dong.Aop.EnableAop;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan("com.dong")
 @EnableAop
 //@EnableAspectJAutoProxy
-public class AppConfig {
+public abstract class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> list) {
+        list.add(new FastJsonHttpMessageConverter());
+
+    }
 }
